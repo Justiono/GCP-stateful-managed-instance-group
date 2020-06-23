7bf7dd1739db915d2797dee2da826cb3dcbf6ebd
Justiono <justiono@yahoo.com.sg>
2020-06-23 15:07:24 -0400

    Update simple.bash

--- simple.bash
+++ simple.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 TEMPLATENAME=`gcloud beta compute instance-groups managed list  --filter="zone~us-east4-c AND name~jpmigvm01" --format="value(instanceTemplate)"`
 CURRVER="${TEMPLATENAME: -1}"
 echo $TEMPLATENAME
