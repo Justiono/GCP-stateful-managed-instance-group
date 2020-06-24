c0ff7518c78460176d18b4fc6056ad460513935d
Justiono <justiono@yahoo.com.sg>
2020-06-24 13:07:35 -0400

    Add files via upload

--- refresh-mig.sh
+++ refresh-mig.sh
@@ -15,7 +15,6 @@ FILEBASE=`basename $0`
 MIGNAME=${VMNAME}-mig
 MIGVER=$(date +%u)
 CURRDATE=$(date +%F)
-LOGFILE=${FILEBASE%.*}-${CURRDATE}.log
 
 gcloud config set project $PROJECTID
 CURRTEMPLATENAME=`gcloud beta compute instance-groups managed list  --filter="zone~${ZONE} AND name~${MIGNAME}" --format="value(instanceTemplate)"`
@@ -32,36 +31,36 @@ MIGTEMPLATE=${VMNAME}-template-$MIGVER
 write_log()
 {
   logmsg=$1
-  echo `date`":"${1} >>$LOGFILE 2>/dev/null
+  echo `date`":"${1} 
 }
 
 write_log "Will be updating to new version of ${MIGVER}"
 
 write_log "Setting default project to ${PROJECTID}"
-gcloud config set project $PROJECTID >>$LOGFILE 2>/dev/null
+gcloud config set project $PROJECTID
 
 create_new_image()
 {
   write_log "Abandoning the instance..."
   gcloud compute instance-groups managed abandon-instances $MIGNAME \
     --instances $VMNAME \
-    --zone=$ZONE >>$LOGFILE 2>&1
+    --zone=$ZONE 
   RC=$?
   write_log "Done. Return code:$RC"
 
   write_log "Stopping the instance..."
-  gcloud compute instances stop $VMNAME --zone=$ZONE >>$LOGFILE 2>&1
+  gcloud compute instances stop $VMNAME --zone=$ZONE 
   RC=$?
   write_log "Done. Return code:$RC"
 
   write_log "Creating new image..."
-  gcloud compute images delete $IMAGENAME --quiet >>$LOGFILE 2>&1
+  gcloud compute images delete $IMAGENAME --quiet 
   RC=$?
   write_log "Done. Return code:$RC"
   write_log "NOTE: it is safe to ignore NOT FOUND error on the first few runs."
   gcloud compute images create $IMAGENAME --source-disk $VMNAME \
     --source-disk-zone $ZONE \
-    --storage-location $REGION >>$LOGFILE 2>&1
+    --storage-location $REGION 
   RC=$?
   if [ ${RC} == "0" ]
   then
@@ -70,7 +69,7 @@ create_new_image()
         write_log "ERR: New image creation failed. Return code:$RC"
         write_log "Manual investigation is required."
         write_log "Starting the old instance..."
-        gcloud compute instances start $VMNAME --zone=$ZONE >>$LOGFILE 2>&1
+        gcloud compute instances start $VMNAME --zone=$ZONE
         RC=$?
         write_log "Exiting with the return code 10..."
 	exit 10  
@@ -80,7 +79,7 @@ create_new_image()
 create_new_template()
 {
   write_log "Creating new template..."
-  gcloud beta compute instance-templates delete $MIGTEMPLATE --quiet >>$LOGFILE 2>&1
+  gcloud beta compute instance-templates delete $MIGTEMPLATE --quiet 
   RC=$?
   write_log "Done. Return code:$RC"
   write_log "NOTE: it is safe to ignore NOT FOUND error on the first few runs."
@@ -93,7 +92,7 @@ create_new_template()
     --disk name=${DISK2},auto-delete=no,device-name=${DISK2},mode=rw \
     --restart-on-failure \
     --region=${REGION} \
-    --network=${VPCNAME} --subnet=${SUBNETNAME} --private-network-ip=${IPADDRESS} --no-address >>$LOGFILE 2>&1
+    --network=${VPCNAME} --subnet=${SUBNETNAME} --private-network-ip=${IPADDRESS} --no-address
   RC=$?
   if [ ${RC} == "0" ]
   then
@@ -102,7 +101,7 @@ create_new_template()
         write_log "ERR: New MIG template creation failed. Return code:$RC"
         write_log "Manual investigation is required."
         write_log "Starting the old instance..."
-        gcloud compute instances start $VMNAME --zone=$ZONE >>$LOGFILE 2>&1
+        gcloud compute instances start $VMNAME --zone=$ZONE 
         RC=$?
         write_log "Exiting with the return code 11..."
         exit 11
@@ -114,13 +113,13 @@ update_instance_group()
   gcloud compute instance-groups managed rolling-action start-update $MIGNAME \
     --version template=${MIGTEMPLATE} \
     --type=opportunistic \
-    --zone $ZONE >>$LOGFILE 2>&1
+    --zone $ZONE 
   RC=$?
   write_log "Done. Return code:$RC"
 
   write_log "Updating instance group with new template..."
   gcloud beta compute instance-groups managed set-instance-template $MIGNAME \
-    --template=${MIGTEMPLATE} --zone $ZONE >>$LOGFILE 2>&1
+    --template=${MIGTEMPLATE} --zone $ZONE 
   RC=$?
   if [ ${RC} == "0" ]
   then
@@ -129,20 +128,20 @@ update_instance_group()
         write_log "ERR: MIG update failed. Return code:$RC"
         write_log "Manual investigation is required."
         write_log "Starting the old instance..."
-        gcloud compute instances start $VMNAME --zone=$ZONE >>$LOGFILE 2>&1
+        gcloud compute instances start $VMNAME --zone=$ZONE 
         RC=$?
         write_log "Exiting with the return code 12..."
         exit 12
   fi
 
   write_log "Cleaning up old instance..."
-  gcloud beta compute instances delete $VMNAME  --project=${PROJECTID}  --zone=${ZONE} --quiet >>$LOGFILE 2>&1
+  gcloud beta compute instances delete $VMNAME  --project=${PROJECTID}  --zone=${ZONE} --quiet 
   RC=$?
   write_log "Done. Return code:$RC"
 
   write_log "Finalizing instance group..."
   gcloud compute --project $PROJECTID instance-groups managed create-instance $MIGNAME \
-    --zone $ZONE --instance $VMNAME >>$LOGFILE 2>&1
+    --zone $ZONE --instance $VMNAME 
   RC=$?
     if [ ${RC} == "0" ]
   then
