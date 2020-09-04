da7fe180c0650b6379a78395ddd6789b74feb53d
Justiono H Putro <justiono@yahoo.com.sg>
2020-09-04 17:36:31 -0400

    Delete simple.bash

--- simple.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TEMPLATENAME=`gcloud beta compute instance-groups managed list  --filter="zone~us-east4-c AND name~jpmigvm01" --format="value(instanceTemplate)"`
-CURRVER="${TEMPLATENAME: -1}"
-echo $TEMPLATENAME
-echo $CURRVER
-gcloud compute instances stop jpaddc02 --zone=northamerica-northeast1-c
